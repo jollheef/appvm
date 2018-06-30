2d8da1ad45f087e262d92c3ad6fc02b4fa2f44d5
Mikhail Klementev <jollheef@riseup.net>
2018-06-30 15:47:54 +0000

    Correct resolution

--- base.nix
+++ base.nix
@@ -11,6 +11,11 @@
     };
     windowManager.xmonad.enable = true;
     windowManager.default = "xmonad";
+    monitorSection = ''
+      Modeline "3840x2160_60.00"  712.75  3840 4160 4576 5312  2160 2163 2168 2237 -hsync +vsync
+      Option "PreferredMode" "3840x2160_60.00"
+      DisplaySize 286 179    # In millimeters
+    '';
   };
 
   users.extraUsers.user = {
