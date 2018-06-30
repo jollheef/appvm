f4aa02f935940d9e5f6103a5b5e9261dab8073b8
Mikhail Klementev <jollheef@riseup.net>
2018-06-30 19:26:48 +0000

    Set real screen size

--- base.nix
+++ base.nix
@@ -14,7 +14,7 @@
     monitorSection = ''
       Modeline "3840x2160_60.00"  712.75  3840 4160 4576 5312  2160 2163 2168 2237 -hsync +vsync
       Option "PreferredMode" "3840x2160_60.00"
-      DisplaySize 286 179    # In millimeters
+      DisplaySize 610 350    # In millimeters
     '';
   };
 
