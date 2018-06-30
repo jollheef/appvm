c58f94d5370e08c089d2c557bbd84d3486ffb599
Mikhail Klementev <jollheef@riseup.net>
2018-06-30 20:01:23 +0000

    Use random port

--- appvm.sh
+++ appvm.sh
@@ -10,7 +10,11 @@ if [[ "$1" == "build" && "$2" != "" ]]; then
     sed "s;NIX_SYSTEM_PLACEHOLDER;${NIX_SYSTEM};" qemu/qemu.template > bin/appvm.${2}
     sed -i "s;NAME_PLACEHOLDER;${2};" bin/appvm.${2}
     sed -i "s;NIX_DISK_IMAGE_PLACEHOLDER;${APPVM_PATH}/qemu/qcow2/${2}.qcow2;" bin/appvm.${2}
-    chmod +x bin/appvm.${2}
+    RANDOM_PORT=$(/usr/bin/python -c 'import random; print(random.randint(1024,65535))')
+    # TODO Check for port collisions
+    sed -i "s;PORT_PLACEHOLDER;${RANDOM_PORT};" bin/appvm.${2}
+    echo -e "#!/bin/bash\nremote-viewer -f spice://127.200.0.1:${RANDOM_PORT}" > bin/appgui.${2}
+    chmod +x bin/app{vm,gui}.${2}
     unlink result
 else
     echo "Usage: $0 build APPLICATION"
