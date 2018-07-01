118255ecdcce6b16dc706a1d015ff6b8e479f473
Mikhail Klementev <jollheef@riseup.net>
2018-07-01 06:51:48 +0000

    Remove monitor section from base

--- nix/base.nix
+++ nix/base.nix
@@ -15,11 +15,6 @@
     };
     windowManager.xmonad.enable = true;
     windowManager.default = "xmonad";
-    monitorSection = ''
-      Modeline "3840x2160_60.00"  712.75  3840 4160 4576 5312  2160 2163 2168 2237 -hsync +vsync
-      Option "PreferredMode" "3840x2160_60.00"
-      DisplaySize 610 350    # In millimeters
-    '';
   };
 
   users.extraUsers.user = {
