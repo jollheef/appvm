1dad75f72afabe18a56df7275f54bba187257042
Mikhail Klementev <jollheef@riseup.net>
2018-07-06 15:06:00 +0000

    Define xmonad config, disable red border

--- nix/base.nix
+++ nix/base.nix
@@ -23,4 +23,24 @@
     extraGroups = [ "audio" ];
     createHome = true;
   };
+
+  environment.etc."xmonad.hs".text = ''
+import XMonad
+main = xmonad defaultConfig
+  { workspaces = [ "" ]
+  , borderWidth = 0
+  }
+  '';
+
+  systemd.services.home-user-build-xmonad = {
+    description = "Create and xmonad configuration";
+    serviceConfig = {
+      ConditionFileNotEmpty = "!/home/user/.xmonad/xmonad.hs";
+      ExecStart = "/bin/sh -c 'mkdir /home/user/.xmonad && cp /etc/xmonad.hs /home/user/.xmonad/xmonad.hs'";
+      RemainAfterExit = "yes";
+      Type = "oneshot";
+      User = "user";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 }
