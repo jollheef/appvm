4493d38947b9ba491136920ceff23122ca85de5f
Mikhail Klementev <jollheef@riseup.net>
2018-07-12 20:42:12 +0000

    Chromium: Install uBlock Origin and HTTPS Everywhere by default

--- nix/chromium.nix
+++ nix/chromium.nix
@@ -5,6 +5,17 @@
     <nix/base.nix>
   ];
 
+  environment.etc."chromium/policies/managed/plugins.json".text = ''
+{
+    "ExtensionInstallForcelist": [
+        // uBlock Origin (https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm)
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx",
+        // HTTPS Everywhere (https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp)
+        "gcbommkclmclpchllfjekcdonpmejbdp;https://clients2.google.com/service/update2/crx",
+    ]
+}
+  '';
+
   environment.systemPackages = [ pkgs.chromium ];
   services.xserver.displayManager.sessionCommands = "while [ 1 ]; do ${pkgs.chromium}/bin/chromium; done &";
 }
