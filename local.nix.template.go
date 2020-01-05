573be660592f7bbbf321ca34f33a2850d8d9baef
Mikhail Klementev <blame@dumpstack.io>
2020-01-05 08:53:57 +0000

    Do no enable anything in default local.nix

--- local.nix.template.go
+++ local.nix.template.go
@@ -2,7 +2,6 @@ package main
 
 var local_nix_template = []byte(`
 {
-  services.xserver.layout = "us,ru";
-  services.xserver.xkbOptions = "ctrl:nocaps,grp:rctrl_toggle";
+  #services.xserver.xkbOptions = "ctrl:nocaps";
 }
 `)
