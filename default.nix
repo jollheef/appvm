19b92aec1bce458483886dd327ac787bba260aa5
Mikhail Klementev <blame@dumpstack.io>
2020-01-22 02:21:19 +0000

    virt-viewer: use name of appvm applications as a title

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ let
     patches = [
       ./patches/0001-Remove-menu-bar.patch
       ./patches/0002-Do-not-grab-keyboard-mouse.patch
+      ./patches/0003-Use-name-of-appvm-applications-as-a-title.patch
     ];
   });
 in with pkgs;
