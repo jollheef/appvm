218e81a6acdc6c2b6337f898dded20fe79a25695
Mikhail Klementev <blame@dumpstack.io>
2020-02-02 02:13:57 +0000

    virt-viewer: use title as subtitle

--- default.nix
+++ default.nix
@@ -5,6 +5,7 @@ let
       ./patches/0001-Remove-menu-bar.patch
       ./patches/0002-Do-not-grab-keyboard-mouse.patch
       ./patches/0003-Use-name-of-appvm-applications-as-a-title.patch
+      ./patches/0004-Use-title-application-name-as-subtitle.patch
     ];
   });
 in with pkgs;
