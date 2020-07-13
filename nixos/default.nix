e2d6c3cbed366dc3704694aecede6e8623de8bdf
Vladimir Serov <me@cab404.ru>
2020-07-14 01:59:17 +0300

    nixos configuration module

---
+++ nixos/default.nix
@@ -0,0 +1,40 @@
+params@{ config, lib, pkgs, ... }:
+let
+  cfg = config.virtualisation.appvm;
+  appvm = import ../. params;
+in with lib; {
+
+  options = {
+    virtualisation.appvm = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          This enables AppVMs and related virtualisation settings.
+        '';
+      };
+      user = mkOption {
+        type = types.str;
+        description = ''
+          AppVM user login. Currenly only AppVMs are supported for a single user only.
+        '';
+      };
+    };
+
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.libvirtd = {
+      enable = true;
+      qemuVerbatimConfig = ''
+        namespaces = []
+        user = "${cfg.user}"
+        group = "users"
+      '';
+    };
+
+    users.users."${cfg.user}".packages = [ appvm ];
+
+  };
+
+}
